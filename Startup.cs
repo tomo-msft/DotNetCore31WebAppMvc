d5936e91557ff5c8ceea57fea54cf00a52e59977
tmasushio <tmasushio@microsoft.com>
2022-10-10 18:23:18 +0900

    Application Insights SDK 追加

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace SampleApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
